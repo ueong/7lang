46bd0469d706bd811a680d66aa41d5d628e19fac
Wooyoung Jung <dnfod@me.com>
2015-12-01 18:40:34 +0900

    haskell

---
+++ haskell/drunken-pirate.hs
@@ -0,0 +1,23 @@
+module Main where
+    stagger :: (Num t) => t -> t
+    stagger d = d + 2
+    crawl d = d + 1
+
+    treasureMap d =
+        crawl (
+        stagger(
+        stagger d))
+
+    letTreasureMap (v, d) = let d1 = stagger d
+                                d2 = stagger d1
+                                d3 = crawl d2
+                             in d3
+                             
+{-
+def treasure_map(v)
+    v = stagger(v)
+    v = stagger(v)
+    v = crawl(v)
+    return( v )
+end
+-}
